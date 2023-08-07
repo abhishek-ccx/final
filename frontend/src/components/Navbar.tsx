6191dfbfd7faec736cd71f8a74f46af5a317147e
abhishekCX099 <abhishek.kumar@changecx.com>
2023-08-07 12:56:17 +0530

    code added

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -9,20 +9,31 @@ import axios from "axios";
 const Navbar = () => {
   const router = useRouter();
   const [userRole, setUserRole] = useState("");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    axios
-      .get("http://127.0.0.1:3000/api/v1/users/getuserrole", {
-        headers: { authorization: localStorage.getItem("token") },
-      })
-      .then((response) => {
-        // console.log(response.data.data.role);
-        setUserRole(response.data.data.role);
-      })
-      .catch((error) => {
-        console.error("Error fetching user role:", error);
-        setUserRole(""); // Set userRole to empty string in case of error
-      });
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      axios
+        .get("http://127.0.0.1:3000/api/v1/users/getuserrole", {
+          headers: { authorization: localStorage.getItem("token") },
+        })
+        .then((response) => {
+          setUserRole(response.data.data.role);
+        })
+        .catch((error) => {
+          console.error("Error fetching user role:", error);
+          setUserRole("");
+        });
+    } else {
+      setUserRole("");
+    }
   }, []);
 
   const handleLogout = (e) => {
@@ -150,15 +161,30 @@ const Navbar = () => {
                     </button>
                   </li>
                   <li>
-                    <Link href="#" className="flex items-center">
+                    <div className="flex items-center">
                       <div className="h-6 w-6">
                         <Image src={user} className="w-full" alt="user-logo" />
                       </div>
-                      <span className="text-black dark:text-black hover:text-orange-400 pl-2">
-                        {/* <button onClick={handleLogout}>Log out</button> */}
-                        <button onClick={handleLogout}>Log out</button>
-                      </span>
-                    </Link>
+                      {isLoggedIn ? (
+                        <div className="pl-2">
+                          <button
+                            className="text-black dark:text-black hover:text-orange-400"
+                            onClick={handleLogout}
+                          >
+                            Log out
+                          </button>
+                        </div>
+                      ) : (
+                        <div className="pl-2">
+                          <Link
+                            href="/login"
+                            className="text-black dark:text-black hover:text-orange-400"
+                          >
+                            Login
+                          </Link>
+                        </div>
+                      )}
+                    </div>
                   </li>
                   <li>
                     <Link href="/cart" className="flex items-center">
